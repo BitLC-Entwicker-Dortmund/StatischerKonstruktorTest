e88883d184021d438a401d71b18d50c58fd35dfa
Klaus <BitLCEntwicklerDortmund@web.de>
2023-04-25 10:58:50 +0200

    Add project files.

---
+++ StatischerKonstruktorTest/Demo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatischerKonstruktorTest {
+    public static class Demo {
+        private static int zahl_1;
+
+        public static void setZahl_1(int z) {
+            zahl_1 = z;
+        }
+
+        public static void printZahl_1() {
+            Console.WriteLine(zahl_1);
+        }
+
+        // Konstruktor
+        static Demo() {
+            Console.WriteLine("Hallo vom Konstruktor");
+            zahl_1 = 999;
+            printZahl_1();
+            Console.WriteLine("Ende des Konstruktors");
+        }
+    }
+}
---
+++ StatischerKonstruktorTest/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace StatischerKonstruktorTest {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, Konstruktor!");
+
+            Demo.setZahl_1(1);
+            Demo.printZahl_1();
+
+            Console.ReadLine();
+        }
+    }
+}
